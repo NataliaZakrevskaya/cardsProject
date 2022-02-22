e36091c964950202e57fbac0dcf50a4bdec00c75
Natalia <zakrevskaya.97@inbox.ru>
2022-02-22 14:59:12 +0300

    Adding Provider to App.tsx

--- src/n1-main/App.tsx
+++ src/n1-main/App.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import './App.css';
 import {Main} from "./Main";
 import {HashRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+import store from "./bll/Store";
 
 function App() {
     return (
         <div className="App">
             {/* hr, provider*/}
             <HashRouter>
-                <Main/>
+                <Provider store={store}>
+                    <Main/>
+                </Provider>
             </HashRouter>
         </div>
     );
