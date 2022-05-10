8bbd58021e6cff127a1c8ea18d689d5560b465ef
Natalia <zakrevskaya.97@inbox.ru>
2022-05-10 13:47:42 +0300

    fixed DeletePackComponent.module.css

--- src/Components/Common/modal/packsModal/DeletePackComponent/DeletePackComponent.tsx
+++ src/Components/Common/modal/packsModal/DeletePackComponent/DeletePackComponent.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import style from '../AddPackComponent/AddPackComponent.module.css';
+import style from './DeletePackComponent.module.css';
+import commonStyle from '../../commonModalStyles.module.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { DeletePackComponentType } from './types';
 import { getIsLoad } from '../../../../../Redux/Selectors/appSelectors/appSelectors';
@@ -19,19 +20,13 @@ const DeletePackComponent = ( { id, setMode }: DeletePackComponentType ) => {
   };
 
   return (
-    <div className={ style.addItemContainer }>
-      <h2>Do you want delete pack ?</h2>
-      <div className={ style.centerInputContainer }>
-        <span>
-          Really ?
-          <span>
-            &nbsp; ✎
-          </span>
-        </span>
-        <div>
-          <button onClick={ onNoButtonClick } disabled={ isLoad }>NO</button>
-          <button onClick={ onYesButtonClick } disabled={ isLoad }>YES</button>
-        </div>
+    <div className={ style.deletePackContainer }>
+      <h2>Delete Pack</h2>
+      <hr/>
+      <p>Do you really want to remove the pack?</p>
+      <div className={ commonStyle.btnGroup }>
+          <button className={commonStyle.cancelBtn} onClick={ onNoButtonClick } disabled={ isLoad }>Cancel</button>
+          <button className={commonStyle.extraActionBtn} onClick={ onYesButtonClick } disabled={ isLoad }>Delete</button>
       </div>
     </div>
   );
